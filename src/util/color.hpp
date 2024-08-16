8ee5c22122985d869fcb6745c6ba11cf2aaba345
Zeroization <1255108389@qq.com>
2024-08-16 14:18:16 +0800

    完成”9.5 Gamma校正“

--- src/util/color.hpp
+++ src/util/color.hpp
@@ -5,10 +5,25 @@
 
 using Color = Vec3;
 
-inline void writeColor(std::ostream& out, const Color& pixel_color) {
+inline double linear_to_gamma(double linear_component)
+{
+	if (linear_component > 0)
+	{
+        return std::sqrt(linear_component);
+	}
+    return 0;
+}
+
+inline void writeColor(std::ostream& out, const Color& pixel_color)
+{
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+
+    // 将数据从线性空间变换到Gamma空间
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // 将属于[0, 1]的RGB分量变换到[0, 255]上
     static const Interval intensity(0.000, 0.999);
